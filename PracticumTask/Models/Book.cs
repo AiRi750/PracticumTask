575878f644e131a3e5e2ab07e30dc2b69a360c95
Aleksei Riabov <omgwtfqqbb@gmail.com>
2021-07-30 16:55:50 +0300

    add Required attribute to models

--- PracticumTask/Models/Book.cs
+++ PracticumTask/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace PracticumTask.Models
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Author { get; set; } //todo - make link to author?
         public string Genre { get; set; } //todo - make link to genre?
