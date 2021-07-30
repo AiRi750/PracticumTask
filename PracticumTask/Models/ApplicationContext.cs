011224f8defb4b2fc716a69f9fbadf58e70652be
Aleksei Riabov <omgwtfqqbb@gmail.com>
2021-07-30 16:34:53 +0300

    add Genre to Db

--- PracticumTask/Models/ApplicationContext.cs
+++ PracticumTask/Models/ApplicationContext.cs
@@ -10,6 +10,7 @@ namespace PracticumTask.Models
     {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
@@ -25,6 +26,10 @@ namespace PracticumTask.Models
         {
             builder.Entity<Book>()
                 .HasIndex(b => b.Title)
+                .IsUnique();
+
+            builder.Entity<Genre>()
+                .HasIndex(g => g.Name)
                 .IsUnique();
 
             //todo - unique author name?
