9b4f12d7311652916cd1faa55db4e6fd085dc6c8
Aleksei Riabov <omgwtfqqbb@gmail.com>
2021-07-30 15:21:41 +0300

    Add ApplicationContext

---
+++ PracticumTask/Models/ApplicationContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticumTask.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=practicum;Username=postgres;Password=1234"); //todo - refactor
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Book>()
+                .HasIndex(b => b.Title)
+                .IsUnique();
+
+            //todo - unique author name?
+        }
+    }
+}
