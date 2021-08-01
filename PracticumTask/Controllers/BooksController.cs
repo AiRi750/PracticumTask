1f259624e60b198d550d07e204a307cfc7eae93b
Aleksei Riabov <omgwtfqqbb@gmail.com>
2021-08-01 23:11:01 +0300

    Add BooksController with GET, POST operations

---
+++ PracticumTask/Controllers/BooksController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PracticumTask.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PracticumTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ApplicationContext context;
+
+        public BooksController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IAsyncEnumerable<Book> Get()
+        {
+            return context.Books;
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Book value)
+        {
+            var book = context.Books.FirstOrDefault(x => x.Title == value.Title);
+            if (book != null)
+                return new ConflictResult();
+
+            context.Add(value);
+            context.SaveChanges();
+            return Ok(context.Books);
+        }
+    }
+}
