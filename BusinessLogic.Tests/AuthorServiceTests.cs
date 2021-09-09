e5ea3af14124120c1045ea4dfed0f865cba1b862
Aleksei Riabov <omgwtfqqbb@gmail.com>
2021-09-09 15:13:46 +0300

    Add BusinessLogic.Tests project

---
+++ BusinessLogic.Tests/AuthorServiceTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace BusinessLogic.Tests
+{
+    [TestFixture]
+    public class AuthorServiceTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
